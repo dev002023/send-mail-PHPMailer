1afc1751bca41c6acb61107bb89e4cdb4af76966
dev002023 <dev002023@gmail.com>
2023-02-05 21:23:57 +0530

    Tested mail

---
+++ index.php
@@ -0,0 +1,38 @@
+<?php
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'vendor/autoload.php';
+
+$mail = new PHPMailer(true);
+
+$pass = "luhnzvzsomkrajvg";
+try {
+    // Server settings
+    $mail->SMTPDebug = 2;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp.gmail.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'useremail@gmail.com';                     // SMTP username
+    $mail->Password   = $pass;                               // SMTP password // Enter your app passwordd from google
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    // Recipients
+    $mail->setFrom('useremail@gmail.com', 'Sender Name');
+    $mail->addAddress('useremail@gmail.com', 'Recipient Name');     // Add a recipient
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Subject Line';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+
+?>
